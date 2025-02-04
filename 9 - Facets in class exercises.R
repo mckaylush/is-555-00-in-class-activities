e5013e43405faed156896438f0ffbb711efc36d4
david-wilson-175 <35510393+david-wilson-175@users.noreply.github.com>
2025-02-04 09:02:46 -0700

    Update 9 - Facets in class exercises.R

--- 9 - Facets in class exercises.R
+++ 9 - Facets in class exercises.R
@@ -15,7 +15,9 @@ movies <- read_csv('https://www.dropbox.com/scl/fi/pi7nexxuoqnvviwfzwun9/movie_r
 
 
 steak <- read_csv('https://www.dropbox.com/scl/fi/mzg5oxenh9oonbwpwgxzm/steak_data.csv?rlkey=2gbf1kfqfkln0zf2alwo32nza&dl=1') %>% 
-  mutate(steak_prep = factor(steak_prep, levels = c('Rare','Medium rare','Medium','Medium Well','Well')))
+  mutate(steak_prep = factor(steak_prep, levels = c('Rare','Medium rare','Medium','Medium Well','Well'))) %>% 
+  mutate(age = factor(age, levels = c('18-29','30-44','45-60','> 60'))) %>% 
+  mutate(hhold_income = factor(hhold_income, levels = c('$0 - $24,999','$25,000 - $49,999','$50,000 - $99,999','$100,000 - $149,999','$150,000+'))) 
 
 
 # Here's a dumb chart:
